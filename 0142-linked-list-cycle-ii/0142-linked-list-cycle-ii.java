828666c71d1387c2e266d572ec751d91f190d1ef
Siddhi-kothekar <121919739+Siddhi-kothekar@users.noreply.github.com>
2025-06-07 22:48:54 +0530

    Time: 0 ms (100%), Space: 44.5 MB (68.07%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -1,33 +1,22 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        Map<ListNode,Integer> mpp = new HashMap<>();
-        ListNode temp = head;
-        int ind = 0;
-        while(temp!=null){
+        ListNode slow = head, fast = head;
 
-            if(mpp.containsKey(temp)){
-                System.out.println("tail connects to node index "+ ind);
-                break;
-            }
-            ind++;
-            mpp.put(temp,ind);
-            temp = temp.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) break;
         }
-        if(temp==null){
-            System.out.println("no cycle");
+
+        if (fast == null || fast.next == null) return null;
+
+        fast = head;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
         }
-        
-        return temp;
+
+        return slow;        
     }
 }
