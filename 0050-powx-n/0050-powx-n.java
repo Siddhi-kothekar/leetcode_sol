65c4a303d1500dd649c2d7b3a6f4354afd50c5e6
Siddhi-kothekar <121919739+Siddhi-kothekar@users.noreply.github.com>
2025-06-27 21:30:06 +0530

    Time: 0 ms (100%), Space: 42.7 MB (28.27%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,26 +1,22 @@
 class Solution {
     public double myPow(double x, int n) {
-        if (n == 0) {
-            return 1; 
+        if(n == Integer.MIN_VALUE){
+            x = 1/x;
+            return myPow(x,Integer.MAX_VALUE) * x;
         }
-
-        if (n == Integer.MIN_VALUE) { 
-            
-            x = 1 / x;
-            return myPow(x, Integer.MAX_VALUE) * x; 
-        }
-
-        if (n < 0) {
-            x = 1 / x;
+        if(n<0){
+            x = 1/x;
             n = -n;
         }
-
-        double half = myPow(x, n / 2);
-        
-        if (n % 2 == 0) {
-            return half * half;  
-        } else {
-            return half * half * x;  
+        double res = 1;
+        double cp = x;
+        while(n>0){
+            if(n % 2 == 1){
+                res = res * cp;
+            }
+            cp = cp * cp;
+            n = n/2;
         }
+        return res;
     }
 }
