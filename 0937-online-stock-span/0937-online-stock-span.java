4c3311b72574de711c80f7abd3b95cfb86f2d3ea
Siddhi-kothekar <121919739+Siddhi-kothekar@users.noreply.github.com>
2025-07-11 16:20:57 +0530

    Time: 31 ms (51.69%), Space: 55.3 MB (68.72%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.java
+++ 0937-online-stock-span/0937-online-stock-span.java
@@ -1,20 +1,30 @@
 class StockSpanner {
     ArrayList<Integer> arr;
+    Stack<Integer> st;
+    int idx ;
     public StockSpanner() {
         arr = new ArrayList<>();
+        st = new Stack<>();
+        idx = 0;
     }
     
     public int next(int price) {
         arr.add(price);
-        int cnt = 1;
-        for(int i = arr.size() - 2;i>=0;i--){
-            if(arr.get(i) <= price){
-                cnt++;
-            }
-            else break;
+        while(!st.isEmpty() && arr.get(st.peek()) <= price){
+            st.pop();
         }
-        return cnt;
+        int span;
+        if(st.isEmpty()){
+            span = idx + 1;
+        }
+        else{
+            span = idx - st.peek();
+        }
+        st.push(idx);
+    idx++;
+    return span;
     }
+    
 }
 
 /**
